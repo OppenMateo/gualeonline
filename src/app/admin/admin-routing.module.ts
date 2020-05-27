c65f598596354ee6633ea317dacc989e6254ff5e
Valetnina <valentina.oppen@southworks.com>
2020-05-27 00:46:02 -0300

    diseño 1

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes =
 [
   {path: '', component: AdminIndexComponent, children:
   [
-    {path: '',redirectTo:'/diseño', pathMatch:'full', component: DatosLugarComponent},
+    {path: '',redirectTo:'diseño', pathMatch:'full', component: DatosLugarComponent},
     {path: 'comercio', component: DatosLugarComponent},
     {path: 'Productos', component: ProductosComponent},
     {path: 'diseño', component: DisenioComponent},
